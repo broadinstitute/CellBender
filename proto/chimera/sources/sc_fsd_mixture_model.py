deb9a81a897140b7183dcbb3acfce56bef35987a
Mehrtash Babadi <mehrtash@broadinstitute.org>
2019-07-30 15:32:12 -0400

    refactoring

--- proto/chimera/sources/sc_fsd_mixture_model.py
+++ proto/chimera/sources/sc_fsd_mixture_model.py
@@ -263,32 +263,33 @@ class SingleCellFamilySizeModel(torch.nn.Module):
         log_p_zero_e_hi_n = torch.nn.functional.logsigmoid(logit_p_zero_e_hi_n)
         log_p_nonzero_e_hi_n = get_log_prob_compl(log_p_zero_e_hi_n)
 
-        # reparameterized Monte-Carlo samples from Gamma(alpha, alpha) for e_hi marginalization
+        # reparameterized Monte-Carlo samples from Gamma(alpha, alpha) for approximate e_hi marginalization
         alpha_e_hi_n = phi_e_hi_n.reciprocal()
         omega_mn = dist.Gamma(concentration=alpha_e_hi_n, rate=alpha_e_hi_n).rsample((n_particles,))
 
-        # contribution of chimeric molecules alone alone
-        log_poisson_e_hi_zero_contrib_n = (
+        # contribution of chimeric molecules alone
+        log_poisson_zero_e_hi_contrib_n = (
                 log_p_zero_e_hi_n
                 + (fingerprint_tensor_nr * log_rate_e_lo_nr).sum(-1)
                 - total_obs_rate_lo_n
                 - fingerprint_log_norm_factor_n)
 
+        # log combined (chimeric and real) Poisson rate for each Gamma particle
         log_rate_combined_mnr = logaddexp(
             log_rate_e_lo_nr,
             log_rate_e_hi_nr + omega_mn.log().unsqueeze(-1))
-        log_poisson_e_hi_nonzero_contrib_mn = (
+        log_poisson_nonzero_e_hi_contrib_mn = (
             (fingerprint_tensor_nr * log_rate_combined_mnr).sum(-1)
             - (total_obs_rate_lo_n + total_obs_rate_hi_n * omega_mn)
             - fingerprint_log_norm_factor_n)
-        log_poisson_e_hi_nonzero_contrib_n = (
-            log_poisson_e_hi_nonzero_contrib_mn.logsumexp(0)
+        log_poisson_nonzero_e_hi_contrib_n = (
+            log_poisson_nonzero_e_hi_contrib_mn.logsumexp(0)
             - np.log(n_particles)
             + log_p_nonzero_e_hi_n)
 
         log_like_n = logaddexp(
-            log_poisson_e_hi_zero_contrib_n,
-            log_poisson_e_hi_nonzero_contrib_n)
+            log_poisson_zero_e_hi_contrib_n,
+            log_poisson_nonzero_e_hi_contrib_n)
 
         return log_like_n
 
