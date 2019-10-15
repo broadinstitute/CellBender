2f3473eabffc8f854434161dc425292399d2bbe5
Mehrtash Babadi <mehrtash@broadinstitute.org>
2019-10-14 20:06:33 -0400

    agnostic initial value for fsd xi

--- proto/chimera/sources/fsd.py
+++ proto/chimera/sources/fsd.py
@@ -438,7 +438,7 @@ class FSDModelGPLVM(FSDModel):
             constraints.greater_than(self.fsd_gplvm_min_noise))
 
         self.fsd_xi_posterior_loc_gq = Parameter(
-            self.init_fsd_xi_loc_posterior.clone().detach()
+            self.init_fsd_xi_loc_prior.clone().detach().expand([sc_fingerprint_dtm.n_genes, self.fsd_xi_dim])
             + self.fsd_init_xi_posterior_scale * torch.randn_like(self.init_fsd_xi_loc_posterior))
         self.fsd_xi_posterior_scale_gq = Parameter(
             self.fsd_init_xi_posterior_scale * torch.ones(
