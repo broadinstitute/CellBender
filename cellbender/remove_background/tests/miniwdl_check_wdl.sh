a554cdbb1bfa5ea469f8fbe7b21ebca838fc8520
Stephen Fleming <sjf11@case.edu>
2023-04-14 15:41:40 -0400

    Only run winiwdl check on the main cellbender WDL

--- cellbender/remove_background/tests/miniwdl_check_wdl.sh
+++ cellbender/remove_background/tests/miniwdl_check_wdl.sh
@@ -5,7 +5,8 @@ set -euxo pipefail
 # runs from the root directory of the repo
 
 # find all WDL files
-WDL_FILES=$(find . -type f -name "*.wdl")
+#WDL_FILES=$(find . -type f -name "*.wdl")
+WDL_FILES=("./wdl/cellbender_remove_background.wdl")
 
 for WDL in ${WDL_FILES}; do
   miniwdl check ${WDL}
