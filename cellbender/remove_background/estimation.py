4f9408d6837de44d8a1b3ab3886acbc9b6f68a82
caelen00000 <direct_your_inquiry_to_caelen@protonmail.com>
2026-06-23 13:16:34 -0500

    convert Mean.estimate_noise to use torch

--- cellbender/remove_background/estimation.py
+++ cellbender/remove_background/estimation.py
@@ -163,6 +163,7 @@ class SingleSample(EstimationMethod):
 class Mean(EstimationMethod):
     """Posterior mean"""
 
+    @torch.no_grad()
     def estimate_noise(
         self, noise_log_prob_coo: sp.coo_matrix, noise_offsets: Optional[Dict[int, int]], device: str = "cpu", **kwargs
     ) -> sp.csr_matrix:
@@ -178,33 +179,29 @@ class Mean(EstimationMethod):
             noise_count_csr: Estimated noise count matrix.
         """
 
-        # TODO: I don't think xp.asnumpy will work if CuPy is not installed
-
         # TODO: noise_log_prob_coo.has_canonical_form is false to begin with, which freaks me out.
         #       noise_log_prob_coo.sum_duplicates() fixes it, but takes a few seconds.
         #       This should probably be called before saving the posterior, but why are there duplicates to begin with?
 
         t0 = time.time()
 
-        xp = cp if device == "cuda" else np
+        c = torch.from_numpy(noise_log_prob_coo.col).to(device=device)
 
-        c = xp.asarray(noise_log_prob_coo.col)
-
-        data = xp.asarray(noise_log_prob_coo.data, copy=True)
-        xp.exp(data, out = data)
+        data = torch.from_numpy(noise_log_prob_coo.data).to(device=device, copy=True)
+        torch.exp(data, out=data)
 
         # Indices are grouped by m.
-        # group_indices is an array of length m that indexes into _ = xp.unique(m) such that _[group_indices] == m.
-        _, group_indices, group_sizes = xp.unique(xp.asarray(noise_log_prob_coo.row),
-                                                  return_inverse=True,
-                                                  return_counts=True)
+        # group_indices is an array of length m that indexes into _ = torch.unique(m) such that _[group_indices] == m.
+        _, group_indices, group_sizes = torch.unique(
+            torch.from_numpy(noise_log_prob_coo.row).to(device=device), return_inverse=True, return_counts=True
+        )
 
         # Gets the mean for each group.
-        # len(group_means) == group_indices.max() + 1 == len(xp.unique(m))
+        # len(group_means) == group_indices.max() + 1 == len(torch.unique(m))
         # Divides by group_sizes since the csr constructor sums data with duplicate coords.
-        group_means = xp.bincount(group_indices, weights=data * c) / group_sizes
+        group_means = torch.bincount(group_indices, weights=data * c) / group_sizes
 
-        # Since group_means is ordered the same as xp.unique(m), we can invert it.
+        # Since group_means is ordered the same as torch.unique(m), we can invert it.
         data_out = group_means[group_indices]
 
         if noise_offsets is not None:
@@ -215,7 +212,7 @@ class Mean(EstimationMethod):
             noise_offsets_n, noise_offsets_g = None, None
 
         new_return = sp.csr_matrix(
-            (xp.asnumpy(data_out) if device == "cuda" else data_out,
+            (data_out.numpy(force=True) if device == "cuda" else data_out,
              self.index_converter.get_ng_indices(noise_log_prob_coo.row)),
             shape=self.index_converter.matrix_shape,
             dtype=np.float32,
