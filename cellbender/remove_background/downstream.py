57fc3acd3c1c3c94a3a8990d96c5d9fd1584b06c
Stephen Fleming <sjfleming@users.noreply.github.com>
2026-08-14 00:12:00 -0400

    anndata deprecated dtype

--- cellbender/remove_background/downstream.py
+++ cellbender/remove_background/downstream.py
@@ -80,7 +80,6 @@ def anndata_from_h5(file: str, analyzed_barcodes_only: bool = True) -> anndata.A
         X=X,
         obs={"barcode": d.pop("barcodes").astype(str)},
         var={"gene_name": (d.pop("gene_names") if "gene_names" in d.keys() else d.pop("name")).astype(str)},
-        dtype=X.dtype,
     )
     adata.obs.set_index("barcode", inplace=True)
     adata.var.set_index("gene_name", inplace=True)
